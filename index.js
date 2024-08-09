0ff41129b1bcf96830cc8df9df27b6b22593a9e0
username0x05 <chineatailer@gmail.com>
2024-08-08 21:31:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-Intro
-Compatibility
-Usage
-Credits
-Smooth Anchor Scrolling
-This Smooth Anchor Scrolling pen was written to reduce the bloat of your project and provide a simple and easy solution for projects using Jquery to apply smooth link scrolling throughout your application or website.
+// https://twitter.com/uixmat
 
-Example link to Usage
-
-Compatibility 👍
-Due to leveraging Jquery, this CodePen works well on all Browsers, both Desktop and Mobile also work perfectly.
-
-Usage
-Simply link to any id on your page from within your .nav.
-
-Credits
-This script was written by myself and is intended for public use. please do not sell this script as an individual product. It may be included in your projects that are sold as an entire website or application.
-
-If you like this pen, you can follow me on Twitter.
+function scrollNav() {
+  $('.nav a').click(function(){
+    $(".active").removeClass("active");     
+    $(this).addClass("active");
+    
+    $('html, body').stop().animate({
+      scrollTop: $($(this).attr('href')).offset().top - 160
+    }, 300);
+    return false;
+  });
+}
+scrollNav();
